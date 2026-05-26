64df879f51886811dbd2e6ebc73b0389e87a12a2
Lukasz <rosicki.lukasz@gmail.com>
2026-05-26 15:15:08 +0200

    Header 0.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon'; // 1. Musimy to zaimportować na górze
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lukaszrosicki.github.io',
   base: '/commercestack/',
+  
+  // 2. Icon ląduje w integracjach Astro
+  integrations: [
+    icon()
+  ],
 
+  // 3. Tailwind v4 zostaje tutaj jako plugin Vite
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [
+      tailwindcss()
+    ]
   }
 });
